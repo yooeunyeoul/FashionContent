a0b44d456ac0e1f651d63136a48286422458d87a
brady <brady@hirediversity.club>
2025-03-27 20:54:27 +0900

    feat: Remove unnecessary classes and interfaces
    This commit removes the following unnecessary classes and interfaces:
    
    -   `NoEvent`
    -   `NoEffect`
    -   `BaseMviViewModelNoEF`

--- app/src/main/java/com/example/stylefeed/base/BaseUiContract.kt
+++ app/src/main/java/com/example/stylefeed/base/BaseUiContract.kt
@@ -4,9 +4,4 @@ package com.example.stylefeed.base
 interface BaseUiEvent
 interface BaseUiEffect
 
-object NoEvent : BaseUiEvent
-object NoEffect : BaseUiEffect
-
-typealias BaseMviViewModelNoEF<S> = BaseMviViewModel<S, NoEvent, NoEffect>
-
 
